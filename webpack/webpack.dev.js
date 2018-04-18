fe4ca5e379ce56927c4b1cb56d09d3744668c63c
Yuqing Chen <chenyuqing@caicloud.io>
2018-04-18 19:12:38 +0800

    frame work finished basically

--- webpack/webpack.dev.js
+++ webpack/webpack.dev.js
@@ -5,6 +5,7 @@ const ExtractTextPlugin = require("extract-text-webpack-plugin");
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
 const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 const htmlPlugin = new HtmlWebpackPlugin({
   template: path.resolve(__dirname, '../src/index.html')
@@ -23,7 +24,12 @@ const forkTsChecker = new ForkTsCheckerWebpackPlugin({
 // 超级无敌天坑
 const tsconfigPathResolver = new TsconfigPathsPlugin({
   configFile: path.resolve(__dirname, '../tsconfig.json')
-})
+});
+
+const cleanDist = new CleanWebpackPlugin(['dist/*'], {
+    root: path.resolve(__dirname, '../'),
+    verbose: true,
+});
 
 module.exports = {
 	mode: "development",
@@ -62,6 +68,7 @@ module.exports = {
     plugins:[tsconfigPathResolver]
 	},
 	plugins: [
+    cleanDist,
     htmlPlugin,
     extractSass,
     forkTsChecker,
