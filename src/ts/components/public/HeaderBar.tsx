1da4de5a6a535f82d3f90b530a83b7c8ee2df046
Yuqing Chen <chenyuqing@caicloud.io>
2018-04-27 10:22:52 +0800

    upgrade some packages

--- src/ts/components/public/HeaderBar.tsx
+++ src/ts/components/public/HeaderBar.tsx
@@ -4,7 +4,7 @@ import React from 'react';
 import { Col, Row } from 'antd';
 import { Link } from 'react-router-dom';
 
-const LEFT_PARTS_COL: number= 4;
+const LEFT_PARTS_COL: number = 4;
 const RIGHT_PARTS_COL: number = 6;
 
 class HeaderBar extends React.Component<{}, {}> {
@@ -13,12 +13,12 @@ class HeaderBar extends React.Component<{}, {}> {
     <Row className="mt-header-bar">
       <Col span={LEFT_PARTS_COL}>
         <Link to="/">
-          <h1><img style={{height: '50px'}} src={require("images/matt_logo.jpg")}/></h1>
+          <h1><img style={{height: '50px'}} src={require('images/matt_logo.jpg')}/></h1>
         </Link>
       </Col>
       {/* <Col span={6}>Header1</Col> */}
       <Col
-        span={RIGHT_PARTS_COL} 
+        span={RIGHT_PARTS_COL}
         offset={24 - LEFT_PARTS_COL - RIGHT_PARTS_COL }
       >Header2</Col>
     </Row>);
