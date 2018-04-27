7b9fe59b193d5e2583f4e8073f68d8059c3ea762
Yuqing Chen <chenyuqing@caicloud.io>
2018-04-27 20:53:04 +0800

    modify style of footer text

--- src/ts/components/public/FooterText.tsx
+++ src/ts/components/public/FooterText.tsx
@@ -3,10 +3,15 @@
 import React from 'react';
 import { Icon } from 'antd';
 
-import { COPYRIGHT_TEXT } from 'ts/utils/consts';
+import { COPYRIGHT_TEXT, ANTD_URL } from 'ts/utils/consts';
 
 const FooterText = (props: {}) => (
-  <>{`${COPYRIGHT_TEXT} Powered by`} <Icon type="ant-design"/>{` Ant Design`}</>
+  <>
+    {`${COPYRIGHT_TEXT} Powered by `}
+    <Icon type="ant-design"/>
+    &nbsp;
+    <a onClick={() => {window.open(ANTD_URL);}} className="ant-d-url">{`Ant Design`}</a>
+  </>
 );
 
 export default FooterText;
--- src/ts/utils/consts.ts
+++ src/ts/utils/consts.ts
@@ -3,3 +3,5 @@
 export const COPYRIGHT_TEXT: string = `Copyright © Matt<mr.chenyuqing@live.com>`;
 
 export const REPO_ADDR: string = `https://github.com/yuqingc/freljord`;
+
+export const ANTD_URL : string = `https://ant.design/`;
