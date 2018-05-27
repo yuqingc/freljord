7a4d33a6ae3a1198de4c941932b12ee36e082504
Matt <mr.chenyuqing@live.com>
2018-05-27 22:16:55 +0800

    rename some file

--- src/ts/components/public/common/index.tsx
+++ src/ts/components/public/common/index.tsx
@@ -1,6 +1,6 @@
 // Copyright 2018 Matt<mr.chenyuqing@live.com>
 
 import Paper from './Paper';
-import showBackButtonInHeader from './ShowBackInHeader';
+import showBackButtonInHeader from './showBackButtonInHeader';
 
 export { Paper, showBackButtonInHeader };
