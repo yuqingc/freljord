577a5e99c8b5e6fcec816a2b3da77a164657b717
Yuqing Chen <chenyuqing@caicloud.io>
2018-05-30 18:25:16 +0800

    remove useless code -- the recursive function

--- src/ts/components/frame/AppRouters.tsx
+++ src/ts/components/frame/AppRouters.tsx
@@ -3,12 +3,6 @@
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { connect } from 'react-redux';
 
-import sidebarRouterConfig,
-  {
-    ISidebarRouter,
-    ISidebarRouterWithComponent,
-    ISidebarRouterWithChildren
-  } from 'ts/utils/sidebarRouterConfig';
 import routes, { IMtRoute } from 'ts/routes';
 import Container from './Container';
 import { NotFound } from 'ts/components/public';
@@ -19,38 +13,6 @@ interface IAppRoutersProps {
 }
 
 class AppRouters extends React.Component<IAppRoutersProps, {}> {
-
-  // This method is deprecated and will be deleted in the future
-  // Render routes recursively
-  // If the route is encrypted and the current user is not admin, it will redirect to the NotFound page
-  // Any depth of route should be registered with this function,
-  // but the side bar only renders to the second depth of route at most
-  // public renderRoutesRecursively (configs: ISidebarRouter[]): any {
-  //   const { isLoggedIn } = this.props;
-  //   const isAdmin = isLoggedIn; // for now
-  //   const resultRoutes: any = [];
-  //   (function go (innerConfigs: ISidebarRouter[], parentPath: string): void {
-  //     for (const v of innerConfigs) {
-  //       if (!v.isEncrypted || isAdmin) {
-  //         if ((v as ISidebarRouterWithComponent).component) {
-  //           resultRoutes.push(
-  //             <Route
-  //               key={v.name}
-  //               path={parentPath + v.path}
-  //               exact={!!v.exact}
-  //               component={(v as ISidebarRouterWithComponent).component}
-  //             />
-  //           );
-  //         }
-  //         if ((v as ISidebarRouterWithChildren).children) {
-  //           go((v as ISidebarRouterWithChildren).children, parentPath + v.path);
-  //         }
-  //       }
-  //     }
-  //   })(configs, '');
-
-  //   return resultRoutes;
-  // }
 
   private renderRoutes (routes: IMtRoute[]) {
     const { isLoggedIn } = this.props;
