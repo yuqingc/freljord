387fbbd03b841cc8d5d426313455e0aa8e5b70d2
Yuqing Chen <chenyuqing@caicloud.io>
2018-07-10 17:26:29 +0800

    wip: edit blog form

--- src/ts/components/contents/blogs/EditBlog.tsx
+++ src/ts/components/contents/blogs/EditBlog.tsx
@@ -1,12 +1,22 @@
 // Copyright 2018 Matt<mr.chenyuqing@live.com>
 
+import { Form, Input } from 'antd';
+
 import { Paper } from 'ts/components/public';
 import withBackButtonInHeader from 'ts/utils/withBackButtonInHeader';
+
+const FormItem = Form.Item;
 
 class EditBlog extends React.Component<{}, {}> {
   render () {
     return (
-      <Paper>edit</Paper>
+      <Paper>
+        <Form layout="vertical">
+          <FormItem label="Title">
+            <Input placeholder="Input the title here" />
+          </FormItem>
+        </Form>
+      </Paper>
     );
   }
 }
